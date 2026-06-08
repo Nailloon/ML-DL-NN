393b11457fd6d44d5f7837c25f100aa34436042a
Nailloon <artemfilyakin5@gmail.com>
2026-06-09 02:49:22 +0600

    updated sh script for new folders

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -5,15 +5,15 @@ ROOT_DIR="$(cd "$(dirname "$0")/.." && pwd)"
 cd "$ROOT_DIR"
 
 NOTEBOOKS=(
-  "notebooks/mlp_mnist.ipynb"
-  "notebooks/cnn_cifar10.ipynb"
-  "notebooks/text_vectorization.ipynb"
-  "notebooks/word2vec.ipynb"
-  "notebooks/logistic_regression.ipynb"
-  "notebooks/svm_kernels.ipynb"
-  "notebooks/gradient_boosting.ipynb"
-  "notebooks/gaussian_process.ipynb"
-  "notebooks/matrix_factorization.ipynb"
+  "notebooks/compiter_vision/1_mlp_mnist.ipynb"
+  "notebooks/compiter_vision/2_cnn_cifar10.ipynb"
+  "notebooks/nlp_and_sequences/1_text_vectorization.ipynb"
+  "notebooks/nlp_and_sequences/2_word2vec.ipynb"
+  "notebooks/classic_ml_and_stats/1_logistic_regression.ipynb"
+  "notebooks/classic_ml_and_stats/2_svm_kernels.ipynb"
+  "notebooks/classic_ml_and_stats/3_gradient_boosting.ipynb"
+  "notebooks/classic_ml_and_stats/4_gaussian_process.ipynb"
+  "notebooks/classic_ml_and_stats/5_matrix_factorization.ipynb"
 )
 
 for notebook in "${NOTEBOOKS[@]}"; do
